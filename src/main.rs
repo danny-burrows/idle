557a8d1fb1c8529f7e84327b62550fe268bdbc97
Danny Burrows <dannyburrows@protonmail.com>
2022-06-14 14:54:48 +0100

    Sparkline and progress ui

--- src/main.rs
+++ src/main.rs
@@ -1,9 +1,13 @@
-use std::{io};
+use std::{
+    io, 
+    time::{Duration, Instant}, fmt::format
+};
 use tui::{
     backend::{Backend, CrosstermBackend},
-    widgets::{Block, List, ListItem, ListState, Borders},
-    layout::{Layout, Constraint, Direction},
+    widgets::{Block, List, ListItem, ListState, Borders, LineGauge, Sparkline, Paragraph},
+    layout::{Layout, Alignment, Constraint, Direction},
     style::{Style, Modifier, Color},
+    symbols,
     Terminal,
     Frame
 };
@@ -37,13 +41,24 @@ impl Incrementors {
 }
 
 struct Idle {
+    total_clicks: u64,
+    inc: u64,
+    sparkline_max_length: usize,
+    sparkline_data: Vec<u64>,
     incrementors: Incrementors
+}
+
+impl Idle {
+    fn on_tick(&mut self) {
+        self.sparkline_data.push(self.inc);
+        self.inc = 0;
+    }
 }
 
 fn ui<B: Backend>(f: &mut Frame<B>, app: &mut Idle) {
     let chunks = Layout::default()
         .direction(Direction::Horizontal)
-        .margin(1)
+        .margin(0)
         .constraints(
             [
                 Constraint::Percentage(75),
@@ -51,11 +66,6 @@ fn ui<B: Backend>(f: &mut Frame<B>, app: &mut Idle) {
             ].as_ref()
         )
         .split(f.size());
-        
-    let block = Block::default()
-        .title("Main Block")
-        .borders(Borders::ALL);
-    f.render_widget(block, chunks[0]);
     
     let block = Block::default()
         .title("Sub Block")
@@ -70,12 +80,68 @@ fn ui<B: Backend>(f: &mut Frame<B>, app: &mut Idle) {
         .highlight_symbol(">>");
 
     f.render_stateful_widget(list, chunks[1], &mut app.incrementors.state);
+
+
+    // Left Chunk
+    let main_chunks = Layout::default()
+        .direction(Direction::Vertical)
+        .margin(0)
+        .constraints(
+            [
+                Constraint::Percentage(25),
+                Constraint::Percentage(25),
+                Constraint::Percentage(50),
+            ].as_ref()
+        )
+        .split(chunks[0]);
+
+    let sparkline_width = main_chunks[0].width.into();
+    app.sparkline_max_length = sparkline_width;
+
+
+    if app.sparkline_data.len() > sparkline_width {
+        
+        let kill_old = app.sparkline_data.len() - sparkline_width;
+
+        app.sparkline_data.drain(0..kill_old);
+    }
+
+    let sparkline = Sparkline::default()
+        .block(Block::default().title("Sparkline:"))
+        .style(Style::default().fg(Color::Green))
+        .data(&app.sparkline_data)
+        .bar_set(symbols::bar::NINE_LEVELS);
+    f.render_widget(sparkline, main_chunks[0]);
+
+    let line_gauge = LineGauge::default()
+        .block(Block::default().title("LineGauge:"))
+        .gauge_style(Style::default().fg(Color::Magenta))
+        .line_set(symbols::line::NORMAL)
+        .ratio(0.69);
+    f.render_widget(line_gauge, main_chunks[1]);
+
+    let block = Block::default()
+    .title("Main Block")
+    .borders(Borders::ALL);
+
+    let paragraph = Paragraph::new(format!("Clicks {}", app.total_clicks))
+        .style(Style::default().fg(Color::Red))
+        .block(block)
+        .alignment(Alignment::Center);
+    f.render_widget(paragraph, main_chunks[2]);
+
 }
 
 
 fn run_app<B: Backend>(terminal: &mut Terminal<B>) -> io::Result<()> {
     
+    let tick_rate = Duration::from_millis(250);
+
     let mut app = Idle {
+        total_clicks: 0,
+        inc: 0,
+        sparkline_max_length: 0,
+        sparkline_data: vec![],
         incrementors: Incrementors { 
             list: vec!["Incrementor 1".to_string(), "Incrementor 2".to_string(), "Incrementor 3".to_string()], 
             state: ListState::default() 
@@ -84,18 +150,35 @@ fn run_app<B: Backend>(terminal: &mut Terminal<B>) -> io::Result<()> {
 
     app.incrementors.state.select(Some(0));
     
+    let mut last_tick = Instant::now();
     loop {
         terminal.draw(|frame| ui(frame, &mut app))?;
 
-        if let Event::Key(key) = event::read()? {
+        let timeout = tick_rate
+            .checked_sub(last_tick.elapsed())
+            .unwrap_or_else(|| Duration::from_secs(0));
+        
+        if crossterm::event::poll(timeout)? {
+            if let Event::Key(key) = event::read()? {
+    
+                match key.code {
+                    KeyCode::Char('q') => return Ok(()),
+                    KeyCode::Up => app.incrementors.prev(),
+                    KeyCode::Down => app.incrementors.next(),
+                    KeyCode::Enter => {
+                        let i = app.incrementors.state.selected().unwrap() as u64 + 1; 
 
-            match key.code {
-                KeyCode::Char('q') => return Ok(()),
-                KeyCode::Up => app.incrementors.prev(),
-                KeyCode::Down => app.incrementors.next(),
-                _ => {}
+                        app.inc += i;
+                        app.total_clicks += i;
+                    },
+                    _ => {}
+                }
+    
             }
-
+        }
+        if last_tick.elapsed() >= tick_rate {
+            app.on_tick();
+            last_tick = Instant::now();
         }
     }
 }
